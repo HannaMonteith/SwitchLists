77a577c6e5afcb4b40124766a1b80a0ad18e62f2
HannaMonteith <hmonteit@uvm.edu>
2024-10-30 10:43:34 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+// make lists (arrays)
+const books = ["Emma by Jane Austen", "Braiding Sweetgrass by Robin Wall Kimmerer", "Normal People by Sally Rooney"]
+const movies = ["Kingsman", "10 Things I Hate About You", "Casablanca"]
+// document.get elementbyid
+const books = document.getElementById('books').checked;
+const movies = document.getElementById('movies').checked;
+// if/else
+//show the list 
